79a84e73b4ea65119f7dc4c35e8f694f55bfbaeb
duda-bueno09 <barea.maria@escola.pr.gov.br>
2024-11-21 09:34:20 -0300

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,15 +1,47 @@
-<!DOCTYPE html>
-<html lang="en">
-  <head>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/p5.js/1.9.4/p5.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/p5.js/1.9.4/addons/p5.sound.min.js"></script>
-    <link rel="stylesheet" type="text/css" href="style.css">
-    <meta charset="utf-8" />
+function setup() {
+  createCanvas(400, 400);
+   palavra = palavraAleatoria();
+  
+}
 
-  </head>
-  <body>
-    <main>
-    </main>
-    <script src="sketch.js"></script>
-  </body>
-</html>
+function palavraAleatoria(){
+  let palavras = ["duda", "linda", "bueno"]
+   return random (palavras);
+}
+
+function inicializaCores(){
+  background(220);
+  fill("green");
+  textSize(64);
+  textAlign(CENTER, CENTER);
+}
+  
+function draw() {
+  inicializaCores();
+  let maximo = width;
+  let minimo  = 0;
+   let quantidade = map(mouseX, 0, width, 1, palavra.length);
+  let parcial = palavra.substring(0, quantidade);
+  text(parcial, 200, 200);
+  
+ /* if (mouseX < 50){
+    let palavra = "A";
+    text(palavra, 200, 200)
+  } else if(mouseX < 100) {
+    let palavra = "Am";
+  text(palavra, 200, 200);
+} else if(mouseX < 150) {
+    let palavra = "Ama";
+  text(palavra, 200, 200);
+} else if(mouseX < 200) {
+    let palavra = "Aman";
+  text(palavra, 200, 200);
+} else if(mouseX < 250) {
+    let palavra = "Amand";
+  text(palavra, 200, 200);
+} else if(mouseX < 300) {
+    let palavra = "Amanda";
+  text(palavra, 200, 200);
+} else if(mouseX < 350) {
+} */
+}
